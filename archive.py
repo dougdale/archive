d0faf286aa15412f27ceabce2bb1fa89f5a2e151
Doug Dale <dougadale@gmail.com>
2016-09-27 20:37:39 -0600

    Walk directories

--- archive.py
+++ archive.py
@@ -10,12 +10,23 @@ them from one directory to another.
 
 import sys
 import argparse
+import os
+import re
 
+def main(args):
+    """Main routine when run from command line"""
 
-def main(arguments):
-    """Set up arguments, call body, handle errors"""
+    skipdirs = re.compile(r'\.[^\.\\/]+')
 
+    for directory, subdirs, files in os.walk(args.source):
+        if not skipdirs.match(directory):
+            print('Directory:', directory)
+            for file in files:
+                print('\t', file)
+
+if __name__ == '__main__':
     formatter_class = argparse.ArgumentDefaultsHelpFormatter
+
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=formatter_class)
 
@@ -27,8 +38,7 @@ def main(arguments):
     parser.add_argument('source', help="Source directory")
     parser.add_argument('destination', help="Destination directory")
 
-    args = parser.parse_args(arguments)
+    args = parser.parse_args(sys.argv[1:])
     print(args)
 
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main(args))
